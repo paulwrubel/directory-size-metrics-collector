e7156ddaf8a417642195889c3bc4adbfd09a60a6
Paul Wrubel <pwrubel7@gmail.com>
2021-06-03 03:10:00 -0500

    fixed bug in script

--- docker_volumes.sh
+++ docker_volumes.sh
@@ -4,7 +4,7 @@ sudo ./collector \
     --address $1 \
     --database $2 \
     --directories "/var/lib/docker/volumes" \
-    # --dry \
     --interval "5m" \
     --loglevel trace \
     --reportingdepth 1
+    # --dry \
